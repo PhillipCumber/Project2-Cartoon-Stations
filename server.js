196a7d2ae667aed87a4689d0616227f8695c3819
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-28 12:54:42 -0400

    trying something

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ let app = express()
 let routes = require('./routes/index')
 
 ////
-// let mongoose = require('mongoose')
-// mongoose.connect('process.env.MONGODB_URI//cartoon-stations')
+let mongoose = require('mongoose')
+mongoose.connect('process.env.MONGODB_URI')
 
 ///Middleware
 app.use(express.urlencoded({ extended: true }))
