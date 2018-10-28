dac71e4b157a5eb56b56d7a43d192027bda0a753
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-28 12:09:59 -0400

    added seeds to new project

---
+++ db/seeds.js
@@ -0,0 +1,38 @@
+let mongoose = require('./connection/')
+
+let cartoonNetwork = require('../models/CartoonNetwork')
+
+let cartoonNetworkShows = [
+
+    {
+
+        name: 'Ed, Edd, Eddy',
+        img: '',
+        fav: 'Rolf',
+        rating: 9
+    },
+
+    {
+       
+        name: 'Dexters Lab',
+        img: '',
+        fav: 'Justice Friends',
+        rating: 10
+    },
+
+    {
+       
+        name: 'Courage the Cowardly dog',
+        img: '',
+        fav: 'Courage',
+        rating: 10
+    },
+
+    {
+       
+        name: 'Cow n Chicken',
+        img: '',
+        fav: 'Boneless Chicken',
+        rating: 8
+    },
+]
